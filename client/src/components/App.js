c37ef9bbf7d4da89686059e6d15678127b9e0d69
Ej Myles <ejelymyles@gmail.com>
2024-04-29 05:56:09 -0700

    Add Header Component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './Header';
+
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <Router>
+      <div>
+        <Header />
+        <h1>Project Client</h1>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+function Header(){
+    return (
+        <div className="header">
+            <h1 className="header-title">Welcome to Car Connect!</h1>
+            <h4 className="header-subtitle">Buy, Rent, and Sale your cars!</h4>
+            <NavBar />
+            <hr className="breakline"/>
+        </div>
+    );
+}
+
+export default Header;
